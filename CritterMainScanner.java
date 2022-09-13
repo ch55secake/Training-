3c0969dd71194a787d8cc6faf6635d45c6210a41
Oscar Serantes Mccormack <87881861+ch55secake@users.noreply.github.com>
2022-09-13 19:56:20 +0100

    Added comments

--- CritterMainScanner.java
+++ CritterMainScanner.java
@@ -4,24 +4,24 @@ import java.util.Scanner;
 
 public class CritterMainScanner {
     public static void main(String[] args) {
-        Scanner typeOfCritter = new Scanner(System.in);
+        Scanner typeOfCritter = new Scanner(System.in); // setup to take user inputs
         System.out.println("Pick a critter? A or B:");
-        String userChoice = typeOfCritter.nextLine();
-        System.out.println("You have chosen " + userChoice);
+        String userChoice = typeOfCritter.nextLine(); // storing the user input 
+        System.out.println("You have chosen " + userChoice); // proving to the user what they have chosen 
 
-        Critter critter;
+        Critter critter; // creating object
 
-        if (userChoice.equals("a")) {
-            critter = new Critter("Jim", true, 0, 0, 0, 0);
+        if (userChoice.equals("a")) { 
+            critter = new Critter("Jim", true, 0, 0, 0, 0); // new Critter object
             while (critter.isAlive()) {
                 System.out.println("Critter has low energy and must sleep!");
                 try {
-                    critter.sleep();
-                    Food b = new Bread();
-                    b.applyFood(critter);
+                    critter.sleep();           // commented on in previous file see there 
+                    Food b = new Bread();      //
+                    b.applyFood(critter);      //
                     critter.exercise();
                 } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    e.printStackTrace(); // debug as a way to see the exception error 
                     throw new RuntimeException(e);
 
                 }
@@ -30,8 +30,8 @@ public class CritterMainScanner {
 
 
 
-            } else if (userChoice.equals("b")){
-              Critter_Type_One critter_type_one;
+            } else if (userChoice.equals("b")){ // same method as seen above, see comments 
+              Critter_Type_One critter_type_one; // creating object 
               critter_type_one = new Critter_Type_One("John", true, 0, 0, 1, 0);
                 while (critter_type_one.isAlive()) {
                     System.out.println("Critter has low energy and must sleep!");
