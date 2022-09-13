b2715691b89ba9e496be6240672abff0cfc07403
Oscar Serantes Mccormack <87881861+ch55secake@users.noreply.github.com>
2022-09-13 19:59:25 +0100

    Added comments

--- Food.java
+++ Food.java
@@ -1,7 +1,7 @@
 package Day_2;
 
 public interface Food {
-    void applyFood(Critter critter) throws InterruptedException;
+    void applyFood(Critter critter) throws InterruptedException; // just sets up food interface, basically just an empty method 
 }
 
 
