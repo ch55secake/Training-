c075e601428007d3afa7f18bb98e5ffb076f31aa
Oscar Serantes Mccormack <87881861+ch55secake@users.noreply.github.com>
2022-09-13 19:44:30 +0100

    Update Bread.java

--- Bread.java
+++ Bread.java
@@ -1,14 +1,14 @@
 package Day_2;
 
-public class Bread implements Food{
+public class Bread implements Food{ // new class that implements the interface 
 
-    private String name = "Bread";
+    private String name = "Bread"; // creating elements of the class
 
     private int energy = 2;
 
-    public void applyFood(Critter critter) throws InterruptedException{
-            critter.feed(this);
-            critter.setEnergy(energy++);
+    public void applyFood(Critter critter) throws InterruptedException{ // method that catches exception 
+            critter.feed(this); // feed method, feeds in food 
+            critter.setEnergy(energy++); // adds energy but it could be written as critter.setEnergy(critter.getEnergy() + energy);
 
     }
 
