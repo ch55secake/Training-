a71dfa588ce5db7e59a677bbe7a10fda7afe7805
Oscar Serantes Mccormack <87881861+ch55secake@users.noreply.github.com>
2022-09-13 19:58:49 +0100

    Added comments

--- Critter_Type_One.java
+++ Critter_Type_One.java
@@ -1,17 +1,17 @@
 package Day_2;
 
-public class Critter_Type_One extends Critter {
+public class Critter_Type_One extends Critter { // this class inherits traits from Critter and has its own traits anyhow 
 
     public Critter_Type_One(String name, boolean isAlive, int foodLevel, int tiredNess, int fitness, int energy) {
-        super(name, isAlive, foodLevel, tiredNess, fitness, energy);
+        super(name, isAlive, foodLevel, tiredNess, fitness, energy); // this is a keyword that refers to the parent class which would be Critter
 
     }
 
-    public void sleep() throws InterruptedException {
+    public void sleep() throws InterruptedException { // same methods as seen in other files 
         System.out.println(name + " is going to sleep now!");
         for (int i = 5; i >= 0; i--) {
             Thread.sleep(1000);
-            System.out.println("Snooooooore!");
+            System.out.println("Snooooooore!"); // different sound effects to meet spec 
         }
     }
 
